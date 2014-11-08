c4cefbfa48802f27c3d3e5a7d3abc0f8372f58df
Partall <partall@ukr.net>
2014-11-08 20:58:36 +0200

    commit

--- p14.erl
+++ p14.erl
@@ -1,8 +1,10 @@
 -module(p14).
 -export([dublicate/1]).
-dublicate(Line)->dublicate(Line,[]).
+dublicate(Line)->
+    dublicate(Line,[]).
 
 dublicate([H|T],Res)->
-	dublicate(T,[H,H|Res]);
+    dublicate(T,[H,H|Res]);
 dublicate([],Res)->
-	p05:reverse(Res).
+    p05:reverse(Res).
+
